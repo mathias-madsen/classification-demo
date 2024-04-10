77050f69de5cb0d1f2f17b70bc5ff3d56793d395
mathias <mathias.winther@gmail.com>
2024-04-10 18:56:46 +0200

    abstract away from complexity from testing script

--- test_on_saved_npz.py
+++ test_on_saved_npz.py
@@ -18,20 +18,11 @@ from image_encoding import encode
 from discriminator import BiGaussianDiscriminator
 
 
-
-if __name__ == "__main__":
-
-    parser = ArgumentParser()
-    parser.add_argument("folder",
-                        type=str,
-                        help="folder in which to look for videos")
-    args = parser.parse_args()
-    if not os.path.isdir(args.folder):
-        raise ValueError("%r must be a folder")
-
+def collect_npz_file_paths(folder):
+    """ Compile a {subfolder: npzpath} dict. """
     print("Seaching for .npz files in %r . . ." % (args.folder,))
     filedict = {}
-    for root, _, filenames in os.walk(args.folder):
+    for root, _, filenames in os.walk(folder):
         for fn in filenames:
             if fn.endswith(".npz"):
                 fp = os.path.join(root, fn)
@@ -44,57 +35,85 @@ if __name__ == "__main__":
     for dirname, pathlist in filedict.items():
          print("%r: %s videos" % (dirname, len(pathlist)))
     print()
+    return filedict
+
+
+def load_video(path):
+    """ Load a single video from NPZ, and print some info. """
+    print(path, ". . .", end=" ")
+    with np.load(path) as source:
+        video = source["arr_0"]
+    print("Video shape: %s" % (video.shape,))
+    return video
+
+
+def vid2codes(video):
+    """ Compute the codes of the images in a video snippet. """
+    return np.stack([encode(f) for f in tqdm(video)], axis=0)
+
+
+def split(episodes):
+    " Cut off one episode and concatenate the rest. "
+    test_id = np.random.randint(len(episodes))
+    train_eps = [e for i, e in enumerate(episodes) if i != test_id]
+    test_eps = [e for i, e in enumerate(episodes) if i == test_id]
+    train_frames = np.concatenate(train_eps, axis=0)
+    test_frames = np.concatenate(test_eps, axis=0)
+    return train_frames, test_frames
+    
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser()
+    parser.add_argument("folder",
+                        type=str,
+                        help="folder in which to look for videos")
+    parser.add_argument("--smoothing",
+                        type=int,
+                        help="number of frames to take into account for a decision",
+                        default=5)
+    args = parser.parse_args()
+    if not os.path.isdir(args.folder):
+        raise ValueError("%r must be a folder")
+
+    filedict = collect_npz_file_paths(args.folder)
 
     print("Loading videos . . .")
-    videodict = {}
-    for dirname, pathlist in filedict.items():
-        videodict[dirname] = []
-        for path in pathlist:
-            print(path, ". . .", end=" ")
-            with np.load(path) as source:
-                video = source["arr_0"]
-                videodict[dirname].append(video)
-            print("Video shape: %s" % (video.shape,))
+    videodict = {subfolder: [load_video(p) for p in pathlist]
+                 for subfolder, pathlist in filedict.items()}
     print()
                 
     print("Computing codes . . .")
-    codesdict = {}
-    for dirname, videolist in videodict.items():
-         codesdict[dirname] = []
-         for video in videolist:
-              codes = np.stack([encode(f) for f in tqdm(video)], axis=0)
-              codesdict[dirname].append(codes)
+    codesdict = {folder: [vid2codes(v) for v in videolist]
+                 for folder, videolist in videodict.items()}
     print("Done.\n")
 
-    def split(episodes):
-         " Cut off one episode and concatenate the rest. "
-         test_id = np.random.randint(len(episodes))
-         train_eps = [e for i, e in enumerate(episodes) if i != test_id]
-         test_eps = [e for i, e in enumerate(episodes) if i == test_id]
-         train_frames = np.concatenate(train_eps, axis=0)
-         test_frames = np.concatenate(test_eps, axis=0)
-         return train_frames, test_frames
-    
     class_a_name, class_b_name = filedict.keys()
-    train_a, test_a = split(codesdict[class_a_name])
-    train_b, test_b = split(codesdict[class_b_name])
-    
     discriminator = BiGaussianDiscriminator()
-    discriminator.fit(train_a, train_b)
 
-    scores_a = discriminator(test_a)
-    scores_b = discriminator(test_b)
+    for _ in range(5):
 
-    print("Test results:")
-    print("|         | %r | %r |" % (class_a_name, class_b_name))
-    print("| :-----: | :-----: | :-----: |")
-    print("| %s | %s | %s |" % (class_a_name,
-                                np.sum(scores_a >= 0),
-                                np.sum(scores_a < 0)))
-    print("| %s | %s | %s |" % (class_b_name,
-                                np.sum(scores_b >= 0),
-                                np.sum(scores_b < 0)))
-    print()
+        train_a, test_a = split(codesdict[class_a_name])
+        train_b, test_b = split(codesdict[class_b_name])    
+        discriminator.fit(train_a, train_b)
+
+        framewise_scores_a = discriminator(test_a)
+        framewise_scores_b = discriminator(test_b)
+
+        kernel = np.ones(args.smoothing) / args.smoothing
+        scores_a = np.convolve(framewise_scores_a, kernel)
+        scores_b = np.convolve(framewise_scores_b, kernel)
+
+        print("Test results:")
+        print("|         | %r | %r |" % (class_a_name, class_b_name))
+        print("| :-----: | :-----: | :-----: |")
+        print("| %s | %s | %s |" % (class_a_name,
+                                    np.sum(scores_a >= 0),
+                                    np.sum(scores_a < 0)))
+        print("| %s | %s | %s |" % (class_b_name,
+                                    np.sum(scores_b >= 0),
+                                    np.sum(scores_b < 0)))
+        print()
 
     figure, axes = plt.subplots(figsize=(12, 8))
     axes.hist(scores_a, bins=51, alpha=0.5, label=class_a_name)
@@ -124,11 +143,13 @@ if __name__ == "__main__":
     axes.axis("off")
     axes.set_title("XXX", fontsize=24)
     figure.tight_layout()
+    plt.pause(10.0)
     for name in videodict.keys():
         for codelist, videolist in zip(codesdict[name], videodict[name]):
-            for code, frame in zip(codelist[::5], videolist[::10]):
+            evidence_history = np.array([discriminator(c) for c in codelist])
+            evidence_history = np.convolve(evidence_history, kernel)
+            for frame, evidence in zip(videolist[::5], evidence_history[::5]):
                 panel.set_data(frame)
-                evidence = discriminator(code)
                 color = "black" if evidence < 0 else "blue"
                 axes.set_title("%.1f" % evidence, fontsize=24, color=color)
                 plt.pause(1 / 15)
