a2034732c6e28ca9bad8feead78b91ec9aba5d3a
mathias <mathias.winther@gmail.com>
2024-10-09 21:24:44 +0200

    add command-line argument control

--- run_classification_demo.py
+++ run_classification_demo.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 from matplotlib import pyplot as plt
 from IPython.terminal.embed import InteractiveShellEmbed
 
@@ -7,32 +8,41 @@ from collector import DataCollector
 
 if __name__ == "__main__":
 
+    parser = ArgumentParser()
+
+    str_or_int = lambda x: int(x) if x.isnumeric() else x
+    parser.add_argument("--camera", type=str_or_int, default=0)
+    parser.add_argument("--model", type=str, default="resnet")
+    parser.add_argument("--downsampling", type=int, default=10)
+
+    args, _ = parser.parse_known_args()
+
     shell = InteractiveShellEmbed()
     shell.enable_matplotlib()
 
     plt.ion()
 
-    image_source = 0
-    encoder = "resnet"
-
     print("Setting up camera . . .")
-    if type(image_source) == int:
+    if type(args.camera) == int:
         import cv2 as cv
-        camera = cv.VideoCapture(image_source)
-    else:
+        camera = cv.VideoCapture(args.camera)
+    elif args.camera.lower() == "ximea":
         from ximea_camera import XimeaCamera
         camera = XimeaCamera()
+    else:
+        raise ValueError("Unrecognized camera option %r" % (args.camera,))
 
-    if encoder == "resnet":
+    if args.model == "resnet":
         from image_encoding import ResNet50Encoder
-        encode = ResNet50Encoder()
-    elif encoder == "local":
+        encode = ResNet50Encoder(downsampling_factor=args.downsampling)
+    elif args.model == "local":
         from pretrained import encode
+    else:
+        raise ValueError("Unrecognized model option %r" % (args.model,))
 
-    discriminator = BiGaussianDiscriminator()
     collector = DataCollector(
         image_encoder=encode,
-        discriminator=discriminator,
+        discriminator=BiGaussianDiscriminator(),
         camera=camera,
         )
 
