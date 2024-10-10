6d7088972bad910ae721742073f7c608f664a4cc
mathias <mathias.winther@gmail.com>
2024-10-10 15:18:09 +0200

    add test of discriminator correctness

--- test_discriminator.py
+++ test_discriminator.py
@@ -35,9 +35,23 @@ def test_saving_and_restoring_of_discriminator():
     assert np.allclose(restored.dist_neg.mean, original.dist_neg.mean)
     assert np.allclose(restored.dist_neg.cov, original.dist_neg.cov)
 
+def test_that_discriminator_correctly_distinguishes_separable_classes():
+    length1, length2 = np.random.randint(1000, 2000, size=2)
+    pos_data = np.random.normal(size=(length1, 2)) + 5.0
+    neg_data = np.random.normal(size=(length2, 2)) - 5.0
+    pos_stats = MomentsTracker.fromdata(pos_data)
+    neg_stats = MomentsTracker.fromdata(neg_data)
+    discriminator = BiGaussianDiscriminator()
+    discriminator.fit_with_moments(pos_stats, neg_stats)
+    true_pos_rate = np.mean(discriminator(pos_data) > 0)
+    assert true_pos_rate > 0.99
+    true_neg_rate = np.mean(discriminator(neg_data) < 0)
+    assert true_neg_rate > 0.99
+
 
 if __name__ == "__main__":
 
     test_saving_and_loading_of_discriminator()
     test_saving_and_restoring_of_discriminator()
-    
+    test_that_discriminator_correctly_distinguishes_separable_classes()
+
