a6b734f6c4b164cb91b8cd580afefcb8eaeacbb7
mathias <mathias.winther@gmail.com>
2024-11-05 21:53:33 +0100

    move dataset main block out into a test

---
+++ classification/test_dataset.py
@@ -0,0 +1,67 @@
+import os
+import numpy as np
+from scipy.datasets import face
+from tempfile import TemporaryDirectory
+
+from classification.dataset import EncodingData
+from encoding.pretrained import OnnxModel
+
+
+def test_dataset():
+
+    big_raccoon = face()
+    small_raccoon = big_raccoon[:256, :320, :]
+
+    encoder = OnnxModel()
+
+    with TemporaryDirectory() as tempdir:
+
+        dataset = EncodingData(encoder, tempdir)
+
+        prep = lambda s: os.path.join(tempdir, s)
+
+        assert os.path.isdir(prep("0"))
+        assert os.path.isdir(prep("1"))
+        assert os.path.isfile(prep("model_info.json"))
+        assert not os.path.isfile(prep("class_indices_and_names.csv"))
+
+        dataset.compute_dimensions(small_raccoon)
+        
+        dataset.class_names[0] = "LEFT"
+        dataset.class_names[1] = "RIGHT"
+
+        dataset.save_class_names_to_file()
+        assert os.path.isfile(prep("class_indices_and_names.csv"))
+
+        dataset.initialize_recording(class_index=0)
+        for _ in range(3):
+            dataset.record_frame(small_raccoon)
+        dataset.save_recording()
+        assert len(os.listdir(prep("0"))) == 3
+        assert len(os.listdir(prep("1"))) == 0
+
+        dataset.initialize_recording(class_index=0)
+        for _ in range(3):
+            dataset.record_frame(small_raccoon)
+        dataset.save_recording()
+        assert len(os.listdir(prep("0"))) == 6
+        assert len(os.listdir(prep("1"))) == 0
+
+        dataset.initialize_recording(class_index=1)
+        for _ in range(3):
+            dataset.record_frame(small_raccoon)
+        dataset.save_recording()
+        assert len(os.listdir(prep("0"))) == 6
+        assert len(os.listdir(prep("1"))) == 3
+
+        dataset.initialize_recording(class_index=1)
+        for _ in range(3):
+            dataset.record_frame(small_raccoon)
+        dataset.discard_recording()
+        assert len(os.listdir(prep("0"))) == 6
+        assert len(os.listdir(prep("1"))) == 3
+
+
+if __name__ == "__main__":
+
+    test_dataset()
