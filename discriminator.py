a58aa6ae1aeb3d8edc915282b8efca7c05855893
mathias <mathias.winther@gmail.com>
2024-11-05 08:35:37 +0100

    save raw stats instead of distribution params

--- discriminator.py
+++ discriminator.py
@@ -6,116 +6,125 @@ from gaussians.moments_tracker import combine, combine_covariance_only
 from gaussians import marginal_log_likelihoods as likes
 
 
+def pick_best_combination(positive_stats, negative_stats, verbose=False):
+
+    dim, = positive_stats.mean.shape
+    prior_stats = MomentsTracker(np.zeros(dim), np.eye(dim), 1.0)
+
+    sepvar = likes.marginal_logp_separate_variances(
+        positive_stats,
+        negative_stats,
+        prior_stats,
+        prior_stats,
+        1.0,
+    )
+
+    sharevar = likes.marginal_logp_separate_variances(
+        positive_stats,
+        negative_stats,
+        prior_stats,
+        prior_stats,
+        1.0,
+    )
+
+    sepcov = likes.marginal_logp_separate_covariances(
+        positive_stats,
+        negative_stats,
+        prior_stats,
+        prior_stats,
+        1.0,
+    )
+
+    sharecov = likes.marginal_logp_shared_covariance(
+        positive_stats,
+        negative_stats,
+        prior_stats,
+        prior_stats,
+        1.0,
+    )
+
+    winner = np.argmax([sharevar, sepvar, sharecov, sepcov])
+
+    if verbose:
+
+        print("Marginal log-likelihoods:")
+        print("-------------------------")
+        print("shared diagonal matrix:   ", sharevar)
+        print("two diagonal matrices:    ", sepvar)
+        print("shared covariance matrix: ", sharecov)
+        print("two covariance matrices:  ", sepcov)
+        print()
+
+        if winner == 0:
+            print("Best model: a shared diagonal matrix.\n")
+        elif winner == 1:
+            print("Best model: two separate diagonal matrices.\n")
+        elif winner == 2:
+            print("Best model: a shared covariance matrix.\n")
+        elif winner == 3:
+            print("Best model: two separate covariance matrices.\n")
+        else:
+            raise Exception("Unpected inddex %s" % (winner,))
+
+    wpos = positive_stats.count / (positive_stats.count + negative_stats.count)
+    wneg = negative_stats.count / (positive_stats.count + negative_stats.count)
+    shared_cov = wpos*positive_stats.cov + wneg*negative_stats.cov
+
+    if winner == 0:
+        poscov = np.diag(shared_cov.diagonal())
+        negcov = np.diag(shared_cov.diagonal())
+    elif winner == 1:
+        poscov = np.diag(positive_stats.cov.diagonal())
+        negcov = np.diag(negative_stats.cov.diagonal())
+    elif winner == 2:
+        poscov = shared_cov.copy()
+        negcov = shared_cov.copy()
+    elif winner == 3:
+        poscov = positive_stats.cov
+        negcov = negative_stats.cov
+    else:
+        raise Exception("Unpected inddex %s" % (winner,))
+
+    post_pos = combine([
+        MomentsTracker(positive_stats.mean, poscov, positive_stats.count),
+        MomentsTracker(np.zeros(dim), np.eye(dim), 1.0),
+    ])
+
+    post_neg = combine([
+        MomentsTracker(negative_stats.mean, negcov, negative_stats.count),
+        MomentsTracker(np.zeros(dim), np.eye(dim), 1.0),
+    ])
+
+    assert not np.isinf(np.linalg.slogdet(post_pos.cov)[1])
+    assert not np.isinf(np.linalg.slogdet(post_neg.cov)[1])
+
+    return post_pos, post_neg
+
+
 class BiGaussianDiscriminator:
 
     def __init__(self):
+        # the raw empirical stats of the data used to train the class models:
+        self.stats_pos = None
+        self.stats_neg = None
+        # the distributions when taking into account the prior and the relative
+        # likelihoods of the different model variants in the light of the data:
         self.dist_pos = None
         self.dist_neg = None
     
     def fit(self, positive_examples, negative_examples, verbose=False):
-        
-        positive_stats = MomentsTracker.fromdata(positive_examples)
-        negative_stats = MomentsTracker.fromdata(negative_examples)
-        self.fit_with_moments(positive_stats, negative_stats, verbose=verbose)
-        del positive_stats
-        del negative_stats
+        self.stats_pos = MomentsTracker.fromdata(positive_examples)
+        self.stats_neg = MomentsTracker.fromdata(negative_examples)
+        self.fit_with_moments(self.stats_pos, self.stats_neg, verbose=verbose)
 
     def fit_with_moments(self, positive_stats, negative_stats, verbose=False):
-
-        dim, = positive_stats.mean.shape
-
-        # prior_stats = MomentsTracker(np.zeros(dim), np.eye(dim), dim)
-        prior_stats = MomentsTracker(np.zeros(dim), np.eye(dim), 1.0)
-
-        sepvar = likes.marginal_logp_separate_variances(
+        self.stats_pos = positive_stats
+        self.stats_neg = negative_stats
+        post_pos, post_neg = pick_best_combination(
             positive_stats,
             negative_stats,
-            prior_stats,
-            prior_stats,
-            1.0,
-        )
-
-        sharevar = likes.marginal_logp_separate_variances(
-            positive_stats,
-            negative_stats,
-            prior_stats,
-            prior_stats,
-            1.0,
-        )
-
-        sepcov = likes.marginal_logp_separate_covariances(
-            positive_stats,
-            negative_stats,
-            prior_stats,
-            prior_stats,
-            1.0,
-        )
-
-        sharecov = likes.marginal_logp_shared_covariance(
-            positive_stats,
-            negative_stats,
-            prior_stats,
-            prior_stats,
-            1.0,
-        )
-
-        winner = np.argmax([sharevar, sepvar, sharecov, sepcov])
-
-        if verbose:
-
-            print("Marginal log-likelihoods:")
-            print("-------------------------")
-            print("shared diagonal matrix:   ", sharevar)
-            print("two diagonal matrices:    ", sepvar)
-            print("shared covariance matrix: ", sharecov)
-            print("two covariance matrices:  ", sepcov)
-            print()
-
-            if winner == 0:
-                print("Best model: a shared diagonal matrix.\n")
-            elif winner == 1:
-                print("Best model: two separate diagonal matrices.\n")
-            elif winner == 2:
-                print("Best model: a shared covariance matrix.\n")
-            elif winner == 3:
-                print("Best model: two separate covariance matrices.\n")
-            else:
-                raise Exception("Unpected inddex %s" % (winner,))
-
-        shared_cov = combine_covariance_only(*positive_stats, *negative_stats)
-
-        if winner == 0:
-            poscov = np.diag(shared_cov.diagonal())
-            negcov = np.diag(shared_cov.diagonal())
-        elif winner == 1:
-            poscov = np.diag(positive_stats.cov.diagonal())
-            negcov = np.diag(negative_stats.cov.diagonal())
-        elif winner == 2:
-            poscov = shared_cov.copy()
-            negcov = shared_cov.copy()
-        elif winner == 3:
-            poscov = positive_stats.cov
-            negcov = negative_stats.cov
-        else:
-            raise Exception("Unpected inddex %s" % (winner,))
-
-        post_pos = combine([
-            MomentsTracker(positive_stats.mean, poscov, positive_stats.count),
-            MomentsTracker(np.zeros(dim), np.eye(dim), 1.0),
-        ])
-
-        post_neg = combine([
-            MomentsTracker(negative_stats.mean, negcov, negative_stats.count),
-            MomentsTracker(np.zeros(dim), np.eye(dim), 1.0),
-        ])
-
-        _, posdet = np.linalg.slogdet(post_pos.cov)
-        _, negdet = np.linalg.slogdet(post_neg.cov)
-        assert not np.isinf(posdet)
-        assert not np.isinf(negdet)
-
-        del self.dist_pos
-        del self.dist_neg
+            verbose=verbose,
+            )
         self.dist_pos = MultivariateNormal(post_pos.mean, post_pos.cov)
         self.dist_neg = MultivariateNormal(post_neg.mean, post_neg.cov)
 
@@ -126,21 +135,25 @@ class BiGaussianDiscriminator:
             return self.dist_pos.logpdf(x) - self.dist_neg.logpdf(x)
     
     def save(self, path):
-        if self.dist_neg is None or self.dist_pos is None:
-            raise ValueError("No parameters to save")
-        dists = [self.dist_neg, self.dist_pos]  # CANONICAL ORDER
-        cluster_means = np.stack([d.mean for d in dists], axis=0)
-        cluster_covs = np.stack([d.cov for d in dists], axis=0)
-        np.savez(path, cluster_means=cluster_means, cluster_covs=cluster_covs)
+        if self.stats_neg is None or self.stats_pos is None:
+            raise ValueError("No stats to save")
+        stats_list = [
+            self.stats_neg,
+            self.stats_pos,
+            ]
+        means = np.stack([m.mean for m in stats_list], axis=0)
+        covs = np.stack([m.cov for m in stats_list], axis=0)
+        counts = np.stack([m.count for m in stats_list], axis=0)
+        np.savez(path, means=means, covs=covs, counts=counts)
 
     def load(self, path):
         with np.load(path) as archive:
-            cluster_means = archive["cluster_means"]
-            cluster_covs = archive["cluster_covs"]
-        # NEGATIVE is number 1 (1-based):
-        self.dist_neg = MultivariateNormal(cluster_means[0], cluster_covs[0])
-        # POSITIVE is number 2 (1-based):
-        self.dist_pos = MultivariateNormal(cluster_means[1], cluster_covs[1])
+            means = archive["means"]
+            covs = archive["covs"]
+            counts = archive["counts"]
+        stats_neg = MomentsTracker(means[0], covs[0], counts[0])
+        stats_pos = MomentsTracker(means[1], covs[1], counts[1])
+        self.fit_with_moments(stats_pos, stats_neg)
 
     @classmethod
     def fromsaved(self, path):
