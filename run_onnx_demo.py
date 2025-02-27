35d13396925df7d99017881fe947bdebf586bc0f
mathias <mathias.winther@gmail.com>
2025-02-27 09:03:29 +0100

    measure times in onnx demo

--- run_onnx_demo.py
+++ run_onnx_demo.py
@@ -2,14 +2,15 @@ import os
 import numpy as np
 import cv2 as cv
 import torch
-from PIL import Image
 from matplotlib import pyplot as plt
+from time import perf_counter
+
 from encoding.pretrained import OnnxModel
 
 
 if __name__ == "__main__":
 
-    wrapper = OnnxModel(downsampling_factor=None)
+    wrapper = OnnxModel(downsampling_factor=5)
     cam = cv.VideoCapture(0)
     
     result, bgr = cam.read()
@@ -18,9 +19,11 @@ if __name__ == "__main__":
 
     plt.ion()
 
-    figure, (left, right) = plt.subplots(figsize=(12, 6),
-                                         ncols=2,
-                                         width_ratios=(5, 1))
+    figure, (left, right) = plt.subplots(
+        figsize=(12, 6),
+        ncols=2,
+        width_ratios=(5, 1),
+        )
 
     cam_display = left.imshow(rgb[:, ::-1, :])
     left.axis("off")
@@ -33,18 +36,21 @@ if __name__ == "__main__":
     figure.tight_layout()
 
     all_encodings = []
-
+    all_durations = []
+    clicks = []
+    loop_start = perf_counter()
     while plt.fignum_exists(figure.number):
-
+        start = perf_counter()
         result, bgr = cam.read()
         rgb = bgr[:, :, ::-1]
         encoding = wrapper(rgb)
         all_encodings.append(encoding)
-
         cam_display.set_data(rgb[:, ::-1, :])
         dot_plot.set_data(encoding, range(len(encoding)))
-
-        plt.pause(1 / 12)
+        plt.pause(1e-4)
+        dur = perf_counter() - start
+        all_durations.append(1000.0 * dur)
+        clicks.append(perf_counter() - loop_start)
 
     means = np.mean(all_encodings, axis=0)
     vars = np.var(all_encodings, axis=0)
